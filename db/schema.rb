7191eca3d18daa0d323a9625131c61dc5e7d448d
Dmitry Kabanov <dkabanov6@gmail.com>
2021-12-24 13:34:14 +0500

    added category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_080812) do
+ActiveRecord::Schema.define(version: 2021_12_24_080527) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%i[Transport Life Work Hobby].each do |category|
+  Category.create(name: category)
+end
