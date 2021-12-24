e4843bbdb2610f8f3bef92ca646f5ff7877b4c5b
Dmitry Kabanov <dkabanov6@gmail.com>
2021-12-24 12:54:01 +0500

    fix tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,12 +4,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test 'should get create' do
-    get sessions_create_url
-    assert_response :success
-  end
-
-  test 'should get destroy' do
-    get sessions_destroy_url
+    get new_session_path
     assert_response :success
   end
 end
