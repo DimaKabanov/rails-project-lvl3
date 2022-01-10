6afc701632f098c25e79531e2285d0d26804dc26
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2022-01-11 00:56:45 +0300

    Remove extra method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,4 @@ class User < ApplicationRecord
   has_many :bulletins, dependent: :destroy
 
   validates :email, presence: true
-
-  def self.find_or_create_by_auth(auth)
-    find_or_initialize_by(email: auth.info.email)
-  end
 end
